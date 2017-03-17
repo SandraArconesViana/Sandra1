cb05914cf1d879f77c084a1154d4a1483ae1481b
SandraArconesViana <sandrita_av@hotmail.com>
2017-03-17 19:15:16 +0100

    Inicio de proyecto

---
+++ src/sandra1/Sandra1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sandra1;
+
+/**
+ *
+ * @author sandra
+ */
+public class Sandra1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola sandra");
+        // TODO code application logic here
+    }
+    
+}
