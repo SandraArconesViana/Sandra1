0e11820e8c1252dea383b0973ccefa9a8a7fb2ce
SandraArconesViana <sandrita_av@hotmail.com>
2017-03-17 19:23:33 +0100

    añadida linea 2

--- src/sandra1/Sandra1.java
+++ src/sandra1/Sandra1.java
@@ -16,6 +16,7 @@ public class Sandra1 {
      */
     public static void main(String[] args) {
         System.out.println("Hola sandra");
+        System.out.println("Hola sandra2");
         // TODO code application logic here
     }
     
