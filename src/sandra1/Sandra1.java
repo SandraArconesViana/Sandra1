c79b7089b2d8ae72d936793a4f89d291802b22af
AitorRammstein <aitor_flash@hotmail.com>
2017-03-18 19:41:42 +0100

    Añadida linea 5

--- src/sandra1/Sandra1.java
+++ src/sandra1/Sandra1.java
@@ -18,6 +18,7 @@ public class Sandra1 {
         System.out.println("Hola sandra");
         System.out.println("Hola sandra2");
         System.out.println("Hola");
+        System.out.println("adios");
         // TODO code application logic here
     }
     
